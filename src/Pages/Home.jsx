4b060601ba3cd9ff21b32c125fe996c5d84d4700
Ananya Singh <ananya18072006@gmail.com>
2026-02-21 11:11:33 +0530

    Home page and styling

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import PokemonCard from ".../components/PokemonCard";
+
+function Home() {
+    const [pokemonList, setPokemonList] = useState([]);
+
+    useEffect(() => {
+        async function fetchPokemon() {
+            const res = await fetch(
+                "https://pokeapi.co/api/v2/pokemon?limit=12"
+            );
+            const data = await res.json();
+
+            const detailedData = await Promise.all(
+                data.results.map(async (pokemon) => {
+                    const response = await fetch(pokemon.url);
+                    return response.json();
+                })
+            );
+
+            setPokemonList(detailedData);
+        }
+
+        fetchPokemon();
+    }, []);
+
+    return (
+        <div className="container">
+            {pokemonList.map((pokemon) => (
+                <PokemonCard
+                    key={pokemon.id}
+                    pokemon={pokemon}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Home;
